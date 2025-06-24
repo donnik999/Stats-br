8aa37c2f6576cae426a762a4854a481ab4456683
donnik999 <denissheynfeld2007@gmail.com>
2025-06-24 09:18:22 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,16 @@
-import logging
+import os
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN = os.getenv("8124119601:AAEgnFwCalzIKU15uHpIyWlCRbu4wvNEAUw")
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start", "help"])
+async def welcome(message: types.Message):
+    await message.reply("Бот работает!")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True) logging
 import re
 import requests
 from aiogram import Bot, Dispatcher, executor, types
